475ed93c60a769e1df6beaac99dbeeedbe7a3664
ratneshkumar028 <ratuyadv7108@gmail.com>
2026-03-10 20:44:10 +0530

    Subarrays with First Element Minimum

---
+++ day1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr) {
+        // code here
+        stack<int> st;
+        int n = arr.size();
+        int res = 0;
+        for(int i =0;i<n;i++){
+            while(!st.empty()&&arr[st.top()]>arr[i]){
+                st.pop();
+                
+            }
+            st.push(i);
+            res +=st.size();
+        }
+        return res;
+    }
+};
